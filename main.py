ea2b70fb8fe0b31e18984341047fb597ac0158bd
Yann-Baelden <yannougdar@yahoo.fr>
2025-02-05 21:02:04 +0100

    B : add fps management

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get(): # close the window's game 
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0, 0, 0))
+        screen.fill("black") # ou pygame.Surface.fill(screen,(0, 0, 0))
         pygame.display.flip()
-    print("Starting asteroids!")
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
